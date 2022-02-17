d5bb3b185818848828df3087730e4a55c6fb9489
Tarik <t.rock22@icloud.com>
2022-02-17 13:56:02 -0500

    dates fixed

--- GroupProjectLibrary/Library.cs
+++ GroupProjectLibrary/Library.cs
@@ -6,19 +6,19 @@ namespace GroupProjectLibrary
 		public List<Book> books = new List<Book>() 
 		{
 
-            new Book ("The Prophet", "Kahlil Gibran", true, DateOnly.Parse("01/03/22")),
+            new Book ("The Prophet", "Kahlil Gibran", true,  DateOnly.FromDateTime(DateTime.Now)),
 			new Book ("The Midnight Library", "Matt Haig", false),
 			new Book ("Black Cake", "Charmaine Wilkerson", false),
-			new Book ("The Maid", "Nita Prose", true, DateOnly.Parse("12/31/22")),
-			new Book ("The Lincoln Highway", "Amor Towles", true, DateOnly.Parse("4/5/22")),
-			new Book ("The Christie Affair", "Nina De Gramont", true, DateOnly.Parse("12/31/22")),
+			new Book ("The Maid", "Nita Prose", true,  DateOnly.FromDateTime(DateTime.Now)),
+			new Book ("The Lincoln Highway", "Amor Towles", true,  DateOnly.FromDateTime(DateTime.Now)),
+			new Book ("The Christie Affair", "Nina De Gramont", true,  DateOnly.FromDateTime(DateTime.Now)),
 			new Book ("The Last Thing He Told Me", "Laura Dave", false),
 			new Book ("The Judge's List", "John Grisham", false),
-			new Book ("Harry Potter and the Chamber of Secrets", "J.K Rowling", true, DateOnly.Parse("6/21/22")),
-			new Book ("The Hobbit", "J.R. Tolkein", true, DateOnly.Parse("5/5/22")),
+			new Book ("Harry Potter and the Chamber of Secrets", "J.K Rowling", true,  DateOnly.FromDateTime(DateTime.Now)),
+			new Book ("The Hobbit", "J.R. Tolkein", true,  DateOnly.FromDateTime(DateTime.Now)),
 			new Book ("It", "Stephen King", false),
 			new Book ("To the Lighthouse", "Virginia Wolfe", false),
-			new Book ("The Lovely Bones", "Alice Sebold", true, DateOnly.Parse("12/31/22")),
+			new Book ("The Lovely Bones", "Alice Sebold", true,  DateOnly.FromDateTime(DateTime.Now)),
 		};
 
 
